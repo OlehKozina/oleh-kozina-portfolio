9618c2f9eedbd37bcb15cb50f53a08c6c11c032b
Oleh Kozina <olegkozina@icloud.com>
2025-11-24 18:20:21 -0800

    updates marquee

--- app/components/Marquee/Marquee.tsx
+++ app/components/Marquee/Marquee.tsx
@@ -10,7 +10,7 @@ function MarqueeWithText({
   if (!projects) return null;
   return (
     <section className="my-10">
-      <Marquee gradient={false} speed={50} className="py-10">
+      <Marquee gradient={false} speed={50} className="py-10 flex ">
         {!!projects?.length &&
           projects.map((item, i) => (
             <a
@@ -18,10 +18,10 @@ function MarqueeWithText({
               target="_blank"
               rel="noopener noreferrer"
               key={i}
-              className="flex items-center mr-8 hover:scale-105 transition-transform border-button bg-button p-4 rounded-3xl overflow-hidden bg-opacity-80"
+              className="flex items-center mr-8 hover:scale-105 transition-transform border-button bg-button p-4 rounded-3xl overflow-hidden bg-opacity-80 h-[10rem]"
             >
               <Image
-                className="h-full rounded-3xl"
+                className="h-full w-auto rounded-3xl object-contain"
                 src={item?.image || ""}
                 width={400}
                 height={400}
